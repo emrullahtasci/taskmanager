486c0f67d239f9f346c59f6b653b565a67867573
Emrullah <emrullah@student.42.fr>
2026-06-02 13:34:53 +0300

    change the GiftItemRepository.java

--- src/main/java/com/example/taskmanager/repository/GiftItemRepository.java
+++ src/main/java/com/example/taskmanager/repository/GiftItemRepository.java
@@ -1,4 +1,13 @@
 package com.example.taskmanager.repository;
 
-public class GiftItemRepository {
+import com.example.taskmanager.entity.GiftItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface GiftItemRepository extends JpaRepository<GiftItem, Long> {
+    List<GiftItem> findByPriceLessThanEqual(double price);
+    List<GiftItem> findByCustomerGroupIgnoreCase(String customerGroup);
+    List<GiftItem> findByNameContainingIgnoreCase(String name);
 }
